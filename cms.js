9fe43c9c81455a884effb91ee611d5e0bcdb36fe
Alex Dollard <alexdollard141@gmail.com>
2020-01-23 15:13:30 -0600

    got first two joins working to display table on running the js file

--- cms.js
+++ cms.js
@@ -1,6 +1,6 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
-let console_table = require("console.table");
+let cTable = require("console.table");
 let SQL_info = require("./SQL_info");
 
 // create the connection information for the sql database
@@ -22,4 +22,9 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
+    console.log("Connected to database.");
+    connection.query("SELECT first_name, last_name, title, salary, dept.name FROM employee as emp LEFT JOIN role as role ON emp.role_id=role.id INNER JOIN department as dept ON dept.id=role.department_id", function(err, result) {
+        if (err) throw err;
+        console.table(result);
+    })
   });
